a3eb789c9bc51dcef36330bf2a848ac15f80ba0f
Peter Marikit <peter.marikit@3qdigital.com>
2020-03-24 14:53:30 +0800

    Add preview on page load when is provided

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+    hljs.highlightBlock(document.getElementById('preview'));
     document.getElementById('edit').focus();
 
     function setHeight() {
@@ -9,36 +10,36 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     window.addEventListener('resize', function(event){
         setHeight();
-    });   
+    });
 
-    const ctrlCode = 17; // control key        
+    const ctrlCode = 17; // control key
     const eCode = 69; // edit
     const nCode = 78; // new
     const sCode = 83; // save
     var previousKey = null;
     document.onkeydown = function (e) {
-        e = e || window.event;            
+        e = e || window.event;
         var pressedKey = e.keyCode;
-        if (previousKey === ctrlCode){                
+        if (previousKey === ctrlCode){
             if (pressedKey === sCode){
                 document.getElementById('preview').classList.remove('hidden');
                 document.getElementById('edit').classList.add('hidden');
-                document.getElementById('preview').innerHTML = `<pre>${document.getElementById('edit').value}</pre>`;                
+                document.getElementById('preview').innerHTML = `<pre><code>${document.getElementById('edit').value}</code></pre>`;
 
                 hljs.highlightBlock(document.getElementById('preview')); // See https://highlightjs.org/
 
-                var language = hljs.highlightAuto(document.getElementById('edit').value).language;                
+                var language = hljs.highlightAuto(document.getElementById('edit').value).language;
                 saveSnippet(language, document.getElementById('edit').value)
                 e.preventDefault();
             } else if(pressedKey === eCode) {
                 document.getElementById('preview').classList.add('hidden');
                 document.getElementById('edit').classList.remove('hidden');
                 document.getElementById('edit').focus();
-                e.preventDefault();   
+                e.preventDefault();
             }
         } else
-            previousKey = pressedKey;    
-    }; 
+            previousKey = pressedKey;
+    };
 
     function saveSnippet(language, code) {
         var xhttp = new XMLHttpRequest();
@@ -46,7 +47,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             if (this.readyState == 4 && this.status == 200) {
                 if(this.responseText != 'false') {
                     window.history.pushState("object or string", "Page Title", `/${this.responseText}`);
-                }                
+                }
             }
         };
         xhttp.open("POST", "/save", true);
