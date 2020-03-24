260f7ca5e5bc44843eafd9b48b16dc0877a16112
Peter Marikit <peter.marikit@3qdigital.com>
2020-03-24 14:28:53 +0800

    Add post route to save code snippet

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const {MongoClient} = require('mongodb');
+const randomstring = require("randomstring");
+
+// Database
+const MongoDB = {
+    'url': process.env.DEBUG ? 'mongodb://127.0.0.1:27017': provess.env.MONGODB_URL,
+    'database': 'kodigo-db',
+    'collection': 'kodigo'
+}
+
+// Read json post data triggered by AJAX
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', 'hbs');
@@ -11,6 +25,22 @@ app.use(express.static('files'));
 
 app.get('/', function (req, res) {    
     res.render('home', {layout: false});
+});
+
+app.post('/save', (req, res) => { 
+    MongoClient.connect(MongoDB['url'], (err, client) => {                    
+        const db = client.db(MongoDB['database']);            
+        const key = `${randomstring.generate(5)}.${req.body['language']}`;
+
+        db.collection(MongoDB['collection']).insertOne({key, code: req.body['code']}).then((result) =>{
+            client.close();
+            console.log(`[SUCCESS] ${key} snippet created.`);
+            res.send(key);
+        }).catch((error) => {
+            console.log(`[ERROR] Creating snippet. ${error}`);
+            res.send(false);
+        });
+    });
 });
 
 app.listen(port, () => {
